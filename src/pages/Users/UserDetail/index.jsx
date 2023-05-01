30edf27328ba65d67abb0f20710f3ef3aa12ba48
Ali-GreenHeart <elinemet.isiyev@gmail.com>
2023-05-01 15:56:05 +0400

    add users, userdetail

--- src/pages/Users/UserDetail/index.jsx
+++ src/pages/Users/UserDetail/index.jsx
@@ -1,9 +1,25 @@
+import axios from "axios"
+import { useEffect, useState } from "react"
 import { useParams } from "react-router"
+
+const USERS_URL = `https://jsonplaceholder.typicode.com/users/`
+
 
 const UserDetail = ({ }) => {
     const { id } = useParams()
+    const [userData, setUserData] = useState({})
+
+    useEffect(() => {
+        axios.get(USERS_URL + id).then(({ data }) => {
+            setUserData(data)
+        })
+    }, [])
     return (
-        <div>UserDetail for {id}</div>
+        <>
+            <h1>UserDetails for {userData.name}</h1>
+            <h3>Name {userData.name}</h3>
+            <h3>Company name {userData.company?.name}</h3>
+        </>
     )
 }
 export default UserDetail
--- src/pages/Users/index.jsx
+++ src/pages/Users/index.jsx
@@ -1,6 +1,32 @@
+import axios from "axios"
+import { useState, useEffect } from "react"
+
+const USERS_URL = `https://jsonplaceholder.typicode.com/users`
+const style = { display: 'block', margin: '20px 0' }
 const Users = ({ }) => {
+    const [users, setUsers] = useState([])
+
+    // useEffect(() => {
+    axios.get(USERS_URL).then(({ data }) => {
+        setUsers(data)
+    })
+    // }, [])
+
     return (
-        <div>Users </div>
+        <>
+            {
+                users.length === 0
+                    ?
+                    <p>melumatlar cekilir</p>
+                    :
+                    users.map(({ id, name, username }) => <a
+                        style={style}
+                        key={id}
+                        href={`/users/${id}`}>
+                        {name}({username})
+                    </a>)
+            }
+        </>
     )
 }
 export default Users   
